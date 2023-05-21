1c1155ad15dad332eb2ce6e5b6e08a5f44b7b7da
H3xCat <2304691+h3xcat@users.noreply.github.com>
2023-05-21 01:45:18 +0000

    Optimize GitHub action

---
+++ build-image.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -f "$(pwd)/build-tools/Dockerfile" -t brokenfly-build .
--- build.sh
+++ build.sh
@@ -5,7 +5,12 @@ GIT_VERSION=$(git describe --always --dirty)
 echo "Building version $VERSION-$GIT_VERSION"
 sleep 2
 rm -rf build/*
-docker build -f "$(pwd)/build-tools/Dockerfile" -t brokenfly-build .
+
+
+if [ "$GITHUB_ACTIONS" != "true" ]; then
+    docker build -f "$(pwd)/build-tools/Dockerfile" -t brokenfly-build .
+fi
+
 docker run --rm  -v "$(pwd)/build:/build-artifacts" brokenfly-build seeed_xiao_rp2040 $VERSION $GIT_VERSION
 docker run --rm  -v "$(pwd)/build:/build-artifacts" brokenfly-build waveshare_rp2040_zero $VERSION $GIT_VERSION
 docker run --rm  -v "$(pwd)/build:/build-artifacts" brokenfly-build waveshare_rp2040_one $VERSION $GIT_VERSION
